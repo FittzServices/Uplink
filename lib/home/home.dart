185636613f2cce0a71f7f91e7c77794bc3226170
Adedayo <fittzservices@gmail.com>
2020-11-12 04:27:53 +0200

    testing changes on logo

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -288,7 +288,7 @@ class _HomeState extends State<Home> {
                                 width: 5,
                               ),
                               Text(
-                                'Uplink Digital Institute',
+                                'Uplink Digital Institute somehow',
                                 style: TextStyle(
                                     fontWeight: FontWeight.w500,
                                     color: Colors.black,
